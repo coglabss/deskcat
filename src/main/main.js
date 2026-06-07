38016629e360cb4f3bde06cba5acfe02f60713b3
Ashok Kumar <coglabss@gmail.com>
2026-06-07 15:26:40 +0530

    feat: scaffold Electron app with hello window

---
+++ src/main/main.js
@@ -0,0 +1,14 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: { contextIsolation: true },
+  });
+  win.loadFile(path.join(__dirname, '../renderer/index.html'));
+}
+
+app.whenReady().then(createWindow);
+app.on('window-all-closed', () => app.quit());
---
+++ src/renderer/loop.js
@@ -0,0 +1,1 @@
+document.getElementById('debug').textContent = 'cat alive (stub loop)';
